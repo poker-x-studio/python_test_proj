d6e2b845aa24ba438bc3ea40ff4b7c9fa28f413f
Danny <sunday20061206@gmail.com>
2025-07-13 05:24:40 +0700

    modify

---
+++ test_return_type.py
@@ -0,0 +1,18 @@
+#功能：测试返回类型
+#说明: Python版本 3.13.5
+
+import os
+
+#清除控制台
+os.system("cls")
+
+
+#加操作
+def my_add(a, b) -> int :
+    return a +b 
+
+def main():
+    return 
+
+if __name__ == "__main__":
+    main()
